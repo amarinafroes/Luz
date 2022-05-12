d0c45509633530751c75db50f9be75b987bf1eaf
Marina <mari.froes1507@gmail.com>
2022-05-12 16:27:20 -0300

    queimando a lampada

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const lampada = document.querySelector("#imagem>img");
 const ligar = document.getElementById("on");
 const desligar = document.getElementById("off");
-
+let cont = 0
 const estado = {
     quebrada: false,
-    ligada: false
+    ligada: false,
+    queimada: false
 }
  
 
@@ -17,9 +18,14 @@ lampada.addEventListener("dblclick", (ev) => {
 
 ligar.addEventListener("click", (ev) => {
     if(estado.ligada == false && estado.quebrada == false ){
+        cont += 1
         estado.ligada = true
         lampada.src = "./img/ligada.jpg";
+    }if(estado.queimada == false && cont == 10){
+        estado.queimada = true
+        lampada.src = "./img/quebrada.jpg";
     }
+    
 });
 desligar.addEventListener("click", (ev) => {
     if(estado.ligada == true && estado.quebrada == false ){
